3774c62cd5f1cdbe792ed3f25be470298dcfdd3c
matthewq337 <79734606+matthewq337@users.noreply.github.com>
2021-02-28 16:23:25 -0700

    Update pybot.py

--- pybot.py
+++ pybot.py
@@ -1,38 +1,57 @@
 import discord
 import os
-import randfacts
 #imports discord
 from datetime import date
-today = date.today() 
+today = date.today()
 #import the date
 import discord.ext
 import re
+import json
+import requests
 from discord.ext import commands
-from randfacts import getFact
 client = discord.Client()
-client = commands.Bot(command_prefix = '!')
-from better_profanity import profanity
+client = commands.Bot(command_prefix='!')
+url = "https://useless-facts.sameerkumar.website/api"
+
+response = requests.get(url)
+data = response.text
+parsed = json.loads(data)
+
+
 @client.event
 async def on_ready():
     print('logged in as {0.user}'.format(client))
-    await client.change_presence(status=discord.Status.online, activity =discord.Game("tf2"))
+    await client.change_presence(status=discord.Status.online,
+                                 activity=discord.Game("tf2"))
+
 
 @client.command(pass_context=True)
 async def version(ctx):
-        myEmbed = discord.Embed (title="current Version", description= " version 1.0 :)", color=0x00ff00)
-        myEmbed.add_field( name = "Version Code", value = 'V1.0' , inline=False)
-        myEmbed.add_field( name = "released", value = today , inline=False)
-        myEmbed.set_author (name= "cinemassacres")
-        myEmbed.set_footer (text = "LULW")
-        
-        await ctx.channel.send(embed=myEmbed)
-        
-@client.event
-async def on_message(message):
+    myEmbed = discord.Embed(title="current Version",
+                            description=" version 1.0 :)",
+                            color=0x00ff00)
+    myEmbed.add_field(name="Version Code", value='V1.0', inline=False)
+    myEmbed.add_field(name="released", value=today, inline=False)
+    myEmbed.set_author(name="made by mattq337")
 
-    if message.content.find("!fact") != -1:
-        await message.channel.send(getFact(False)) 
+    await ctx.channel.send(embed=myEmbed)
 
 
+#Admin commands
+@client.command()
+async def clear(ctx, amount=6):
+    await ctx.channel.purge(limit=amount)
 
-client.run(os.environ["DISCORD_TOKEN"])
+
+@client.command()
+async def kick(ctx, member: discord.Member, *, reason=None):
+    await member.kick(reason=reason)
+
+
+@client.command()
+async def ban(ctx, member: discord.Member, *, reason=None):
+    await member.ban(reason=reason)
+
+
+keep_alive()
+client.run(os.environ["TOKEN"])
