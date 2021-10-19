2ba581f82f40e4bf66745b3d023edb03f231aea7
matthewq337 <79734606+matthewq337@users.noreply.github.com>
2021-10-19 12:38:49 -0600

    Update pybot.py

--- pybot.py
+++ pybot.py
@@ -1,102 +1,235 @@
-from datetime import date
-from dotenv import load_dotenv
-import aiohttp
-import time
-from discord.ext import commands
-import requests
-import json
-import random
-import re
-import discord.ext
-import os
-import discord
-from aiohttp.client import request
+
 TOKEN = ""
-# keep all imports here
-# keep all from here
-TOKEN = os.getenv('DISCORD_TOKEN')
-client = discord.Client()
-url = "https://useless-facts.sameerkumar.website/api"
+import json
+import os
+import random
+ 
+import aiohttp
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+ 
 load_dotenv()
-response = requests.get(url)
-data = response.text
-parsed = json.loads(data)
-
-
-def get_prefix(client, message):  # first we define get_prefix
-    # we open and read the prefixes.json, assuming it's in the same file
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)  # load the json as prefixes
-        # recieve the prefix for the guild id given
-        return prefixes[str(message.guild.id)]
-
-
-client = commands.Bot(command_prefix=get_prefix)
-
-
-@client.event
+ 
+ 
+DEFAULT_PREFIX = "!"
+ 
+ 
+class JsonDict(dict):
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+        try:
+            with open(self.file_name, "r") as file:
+                self.update(json.load(file))
+        except FileNotFoundError:
+            pass
+ 
+    def save(self):
+        with open(self.file_name, "w+") as file:
+            json.dump(self, file, indent=2)
+ 
+ 
+prefixes = JsonDict("prefixes.json")
+ 
+ 
+TOKEN = "ODE1MzYyOTkzNTcwOTcxNjQ4.YDrT9g.sDbCVaQ9opmmm6bIymaQUZSXTZM"
+import json
+import os
+import random
+ 
+import aiohttp
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+ 
+load_dotenv()
+ 
+ 
+DEFAULT_PREFIX = "!"
+ 
+ 
+class JsonDict(dict):
+    def __init__(self, file_name: str):
+        self.file_name = file_name
+        try:
+            with open(self.file_name, "r") as file:
+                self.update(json.load(file))
+        except FileNotFoundError:
+            pass
+ 
+    def save(self):
+        with open(self.file_name, "w+") as file:
+            json.dump(self, file, indent=2)
+ 
+ 
+prefixes = JsonDict("prefixes.json")
+ 
+ 
+def get_prefix(bot, message):
+    return prefixes.get(str(message.guild.id), DEFAULT_PREFIX)
+ 
+ 
+bot = commands.Bot(command_prefix=get_prefix)
+ 
+ 
+@bot.event
 async def on_ready():
-    print('logged in as {0.user}'.format(client))
-    await client.change_presence(status=discord.Status.online,
-                                 activity=discord.Game("tf2"))
-
-
-@client.command()
-async def ping(ctx):
-    await ctx.send(f"pong {round(client.latency * 1000)}ms")
-
-@commands.command(aliases=['roll'])
-async def roll(self, ctx):
-        n = random.randint(0,500)
-        author = ctx.message.author
-
-        embed = discord.Embed(
-            colour = discord.Colour.green()
-        )
-
-        embed.set_thumbnail(url='https://cdn.discordapp.com/attachments/762430826755260449/764946212527931392/die.png')
-        embed.add_field(name='Roll', value=(f'Your random number is {n}/500.'), inline=False)
-
-        await ctx.send(embed=embed)
-
-@client.command()
-async def fact(ctx):
+    print(f"logged in as {bot.user}")
+    await bot.change_presence(
+        status=discord.Status.online, activity=discord.Game("tf2")
+    )
+ 
+ 
+@bot.event
+async def on_guild_join(guild):
+    prefixes[str(guild.id)] = DEFAULT_PREFIX
+    prefixes.save()
+ 
+ 
+@bot.command()
+async def ping(ctx: commands.Context):
+    """
+    Returns the bots estimated latency to discord servers
+    """
+    await ctx.send(f"pong {round(bot.latency * 1000)}ms")
+ 
+ 
+@bot.command()
+async def roll(ctx: commands.Context):
+    """
+    Get a random number from 0-500
+    """
+    n = random.randint(0, 500)
+    embed = discord.Embed(colour=discord.Colour.green())
+    embed.set_thumbnail(
+        url="https://cdn.discordapp.com/attachments/762430826755260449/764946212527931392/die.png"
+    )
+    embed.add_field(
+        name="Roll", value=(f"Your random number is {n}/500."), inline=False
+    )
+    await ctx.send(embed=embed)
+ 
+ 
+@bot.command()
+async def fact(ctx: commands.Context):
+    """
+    Returns a random fact
+    """
+    url = "https://useless-facts.sameerkumar.website/api"
     async with aiohttp.ClientSession() as session:
-
-        # This gets the fact request
-        request2 = await session.get("https://useless-facts.sameerkumar.website/api")
-    factjson = await request.json()
-    await ctx.send(factjson.get("data"))
-
-
-@client.command()
+        response = await session.get(url)
+        if response.status == 200:
+            _json = await response.json()
+            await ctx.send(_json.get("data"))
+ 
+ 
+@bot.command()
 async def clear(ctx, amount=6):
+    """
+    Clear messages from a channel
+ 
+    Args:
+        amount (optional): Number of messages to remove. Defaults to 6.
+    """
     await ctx.channel.purge(limit=amount)
-
-
-@client.event
-async def on_guild_join(guild):  # when the bot joins the guild
-    with open('prefixes.json', 'r') as f:  # read the prefix.json file
-        prefixes = json.load(f)  # load the json file
-
-    prefixes[str(guild.id)] = '!'  # default prefix
-
-    with open('prefixes.json', 'w') as f:  # write in the prefix.json "message.guild.id": "!"
-        # the indent is to make everything look a bit neater
-        json.dump(prefixes, f, indent=4)
-
-
-@client.command()
-# ensure that only administrators can use this command
+ 
+ 
 @commands.has_permissions(administrator=True)
-async def changeprefix(ctx, prefix):  # command: !changeprefix ...
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
+@bot.command()
+async def changeprefix(ctx, prefix):
+    """
+    Change the prefix for your guild
+ 
+    Args:
+        prefix: The new prefix to use
+    """
     prefixes[str(ctx.guild.id)] = prefix
-
-    with open('prefixes.json', 'w') as f:  # writes the new prefix into the .json
-        json.dump(prefixes, f, indent=4)
-
-
-client.run(")
+    prefixes.save()
+    await ctx.send(f"Prefix has been changed to `{prefix}`")
+ 
+ 
+bot.run("ODE1MzYyOTkzNTcwOTcxNjQ4.YDrT9g.sDbCVaQ9opmmm6bIymaQUZSXTZM")
+def get_prefix(bot, message):
+    return prefixes.get(str(message.guild.id), DEFAULT_PREFIX)
+ 
+ 
+bot = commands.Bot(command_prefix=get_prefix)
+ 
+ 
+@bot.event
+async def on_ready():
+    print(f"logged in as {bot.user}")
+    await bot.change_presence(
+        status=discord.Status.online, activity=discord.Game("tf2")
+    )
+ 
+ 
+@bot.event
+async def on_guild_join(guild):
+    prefixes[str(guild.id)] = DEFAULT_PREFIX
+    prefixes.save()
+ 
+ 
+@bot.command()
+async def ping(ctx: commands.Context):
+    """
+    Returns the bots estimated latency to discord servers
+    """
+    await ctx.send(f"pong {round(bot.latency * 1000)}ms")
+ 
+ 
+@bot.command()
+async def roll(ctx: commands.Context):
+    """
+    Get a random number from 0-500
+    """
+    n = random.randint(0, 500)
+    embed = discord.Embed(colour=discord.Colour.green())
+    embed.set_thumbnail(
+        url="https://cdn.discordapp.com/attachments/762430826755260449/764946212527931392/die.png"
+    )
+    embed.add_field(
+        name="Roll", value=(f"Your random number is {n}/500."), inline=False
+    )
+    await ctx.send(embed=embed)
+ 
+ 
+@bot.command()
+async def fact(ctx: commands.Context):
+    """
+    Returns a random fact
+    """
+    url = "https://useless-facts.sameerkumar.website/api"
+    async with aiohttp.ClientSession() as session:
+        response = await session.get(url)
+        if response.status == 200:
+            _json = await response.json()
+            await ctx.send(_json.get("data"))
+ 
+ 
+@bot.command()
+async def clear(ctx, amount=6):
+    """
+    Clear messages from a channel
+ 
+    Args:
+        amount (optional): Number of messages to remove. Defaults to 6.
+    """
+    await ctx.channel.purge(limit=amount)
+ 
+ 
+@commands.has_permissions(administrator=True)
+@bot.command()
+async def changeprefix(ctx, prefix):
+    """
+    Change the prefix for your guild
+ 
+    Args:
+        prefix: The new prefix to use
+    """
+    prefixes[str(ctx.guild.id)] = prefix
+    prefixes.save()
+    await ctx.send(f"Prefix has been changed to `{prefix}`")
+ 
+ 
+bot.run("")
