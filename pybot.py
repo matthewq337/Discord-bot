f2dbf8433da2ac7854f7ad0f214f702b451afd28
matt <johndoe@example.com>
2022-02-04 22:08:52 -0700

    added a test

--- pybot.py
+++ pybot.py
@@ -45,7 +45,7 @@ if os.path.exists(os.getcwd() + "/config.json"):
     with open("./config.json") as f:
         configData = json.load(f)
 
-
+h
 
 else:
     configTemplate = {"token": "", "Prefix": "!"}
